6878d819f1b3044a9396b96794f1e8a0553d3056
Pablo Fernandez Hinojosa <pablo.hinojosa@ort.edu.ar>
2025-04-03 20:55:28 -0300

    + resolución de videos

--- ejercicio-videos.js
+++ ejercicio-videos.js
@@ -24,3 +24,32 @@ const str = `<ul>
 // Obtener el total de segundos de los videos de tipo Redux
 // Tip: Obtener un array de objetos, donde cada objeto sea un video
 // {min:5, seg: 59, tipo: 'Redux Video'}
+
+function getVideos(str) {
+  return str
+    .replace("<ul>", "")
+    .replace("</ul>", "")
+    .split("</li>")
+    .slice(0, -1)
+    .map((video) => ({
+      min: parseInt(video.split('"')[1].split(":")[0]),
+      seg: parseInt(video.split('"')[1].split(":")[1]),
+      tipo: video.split(">")[1],
+    }));
+}
+
+// TODO: utilizar filter en lugar del if
+// TODO: utilizar reduce en lugar del foreach, para totalizar
+// find => ubicar el primer elemento que cumple una condición
+function getTotalSegundos(videos, tipo) {
+  let total = 0;
+  videos.forEach((video) => {
+    if (video.tipo == tipo) {
+      total += video.seg;
+    }
+  });
+
+  return total;
+}
+
+console.log(getTotalSegundos(getVideos(str), "Flexbox Video"));
