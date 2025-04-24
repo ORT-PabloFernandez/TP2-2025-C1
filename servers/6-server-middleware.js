c2857e27c4e94ca2412f7adb9eab11a186d2f8b7
Pablo Fernandez Hinojosa <pablo.hinojosa@ort.edu.ar>
2025-04-24 19:51:26 -0300

    + middleware, post

---
+++ servers/6-server-middleware.js
@@ -0,0 +1,53 @@
+import express from "express";
+import chalk from "chalk";
+
+const PORT = 3000;
+
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879 },
+  { first: "Isaac", last: "Newton", year: 1643 },
+  { first: "Galileo", last: "Galilei", year: 1564 },
+  { first: "Marie", last: "Curie", year: 1867 },
+  { first: "Johannes", last: "Kepler", year: 1571 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473 },
+  { first: "Max", last: "Planck", year: 1858 },
+];
+
+const html = `
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>
+            </head>
+            <body>
+                <h3>Esto es la pagina de documentación de la API Inventores</h3>
+            </body>
+            </html>
+        `;
+
+const app = express();
+
+// en express un middlerware es un app.use
+app.use("/", (req, res, next) => {
+  if (req.query.key == "123abc") {
+    next();
+  } else {
+    res.status(401).send("Login fail");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send(html);
+});
+
+app.get("/api/inventors/:id", (req, res) => {
+  console.log("parametro por querystring", req.query.order);
+  console.log("parametro por queryparams", req.params.id);
+  res.json(inventors);
+});
+
+app.listen(PORT, () => {
+  console.log(chalk.bgGreen("Servidor levantado en puerto ", PORT));
+});
